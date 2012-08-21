0681dc65bf073bd306e20bf529b8f67bd0554c16
Taka Kojima <taka.kojima@ff0000.com>
2012-08-21 12:50:51 -0700

    Updating libraries and config.js

--- config.js
+++ config.js
@@ -1,12 +1,38 @@
 require.config({
 	paths: {
-		"jquery": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
-		"$" : "libs/plugins/amd/$"
+		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+		"$plugin" : "libs/plugins/amd/jquery-plugin",
+		"underscore": "libs/lodash",
+		"swig": "libs/swig.min",
+		"History" : "libs/history.min",
+		"html5shiv" : "libs/html5shiv.min",
+		"Modernizr" : "libs/modernizr",
+		"swfobject" : "libs/swfobject",
+		"text" : "libs/plugins/amd/text",
+		"json" : "libs/plugins/amd/json",
+		"templates" : "../../templates"
 	},
 	waitSeconds: 15,
 	shim : {
-		jquery : {
+		"$" : {
 			exports : "jQuery"
+		},
+		"underscore": {
+			deps: [],
+			exports: "_"
+		},
+		"swig": {
+			deps: ["underscore"],
+			exports : "swig"
+		},
+		"History": {
+			exports : "History"
+		},
+		"Modernizr": {
+			exports : "Modernizr"
+		},
+		"swfobject" : {
+			exports : "swfobject"
 		}
 	}
 });
