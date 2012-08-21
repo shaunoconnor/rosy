a2ba4a511abe5a59166c92fc5a3c6d6acbf0bfef
Taka Kojima <taka.kojima@ff0000.com>
2012-08-21 11:32:42 -0700

    No default vars property, adding indexOf polyfill

--- red/base/Class.js
+++ red/base/Class.js
@@ -32,13 +32,31 @@ define(
 			};
 		}
 
+		/*================= Array.indexOf polyfill =================*/
+
+		Array.prototype.indexOf = Array.prototype.indexOf || function (a, b) {
+			if (!this.length || !(this instanceof Array) || arguments.length < 1) {
+				return -1;
+			}
+
+			b = b || 0;
+
+			if (b >= this.length) {
+				return -1;
+			}
+
+			while (b < this.length) {
+				if (this[b] === a) {
+					return b;
+				}
+				b += 1;
+			}
+			return -1;
+		};
+
 		/*==================================*/
 
 		return AbstractClass.extend({
-
-			vars : {
-				USE_VARS: true
-			},
 
 			init : function (vars, context) {},
 
