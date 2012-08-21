2018f510ba823babda2e3abef315f3df10888699
Taka Kojima <taka.kojima@ff0000.com>
2012-08-21 13:32:25 -0700

    Use a barebones build of lodash as the default mapping for underscore

--- config.js
+++ config.js
@@ -1,25 +1,31 @@
 require.config({
+
 	paths: {
 		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
 		"$plugin" : "libs/plugins/amd/jquery-plugin",
-		"underscore": "libs/lodash",
-		"swig": "libs/swig.min",
 		"History" : "libs/history.min",
 		"html5shiv" : "libs/html5shiv.min",
 		"Modernizr" : "libs/modernizr",
 		"swfobject" : "libs/swfobject",
 		"text" : "libs/plugins/amd/text",
 		"json" : "libs/plugins/amd/json",
-		"templates" : "../../templates"
+		"templates" : "../../templates",
+
+		/**
+		* If you are using swig or if you want to use more than _.clone and _.extend:
+		* Replace `libs/lodash.barebones` with `libs/lodash`
+		**/
+
+		"underscore": "libs/lodash.barebones",
+		"swig": "libs/swig.min",
+
 	},
+
 	waitSeconds: 15,
+
 	shim : {
 		"$" : {
 			exports : "jQuery"
-		},
-		"underscore": {
-			deps: [],
-			exports: "_"
 		},
 		"swig": {
 			deps: ["underscore"],
