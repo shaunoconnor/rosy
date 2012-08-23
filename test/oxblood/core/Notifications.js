2c72a7a8e62e9e16272e062bfad92a92d9a86185
Richard Herrera <rich@doctyper.com>
2012-08-22 18:01:34 -0700

    Adding tests for n.hold, n.release, n.cancel, n.respond, n.dispatcher.

--- test/oxblood/core/Notifications.js
+++ test/oxblood/core/Notifications.js
@@ -58,6 +58,136 @@ define([
 					testInstance.publish("pub-test");
 				});
 
+				it("should publish a notification with data", function (done) {
+					testInstance.subscribe("pub-data-test", function (notification) {
+						expect(notification.data).to.deep.equal({
+							x : 1,
+							y : 2
+						});
+
+						done();
+					});
+
+					testInstance.publish("pub-data-test", {
+						x : 1,
+						y : 2
+					});
+				});
+
+			});
+
+			describe(".hold()", function () {
+				var holdInstance = new Page();
+
+				it("should hold a notification", function (done) {
+					var time, lapse;
+					var delay = 50;
+
+					testInstance.subscribe("hold-test", function (notification) {
+						time = new Date().getTime();
+
+						notification.hold();
+
+						window.setTimeout(function () {
+							notification.release();
+						}, delay);
+
+						done();
+					});
+
+					holdInstance.subscribe("hold-test", function (notification) {
+						lapse = new Date().getTime() - time;
+						expect(lapse).to.be.greaterThan(delay - 1);
+
+						done(false);
+					});
+
+					testInstance.publish("hold-test");
+				});
+
+			});
+
+			describe(".release()", function () {
+				var releaseInstance = new Page();
+
+				it("should release a notification", function (done) {
+					var time, lapse;
+					var delay = 50;
+
+					testInstance.subscribe("release-test", function (notification) {
+						time = new Date().getTime();
+
+						notification.hold();
+
+						window.setTimeout(function () {
+							notification.release();
+							done(false);
+						}, delay);
+					});
+
+					releaseInstance.subscribe("release-test", function (notification) {
+						lapse = new Date().getTime() - time;
+						expect(lapse).to.be.greaterThan(delay - 1);
+
+						done();
+					});
+
+					testInstance.publish("release-test");
+				});
+
+			});
+
+			describe(".cancel()", function () {
+				var cancelInstance = new Page();
+
+				it("should cancel a notification", function (done) {
+					testInstance.subscribe("cancel-test", function (notification) {
+						notification.cancel();
+
+						window.setTimeout(function () {
+							done();
+						}, 50);
+					});
+
+					cancelInstance.subscribe("cancel-test", function (notification) {
+						done(false);
+					});
+
+					testInstance.publish("cancel-test");
+				});
+			});
+
+			describe(".respond()", function () {
+				it("should respond to a notification with a callback", function (done) {
+					testInstance.subscribe("respond-test", function (notification) {
+						expect(notification).to.be.an("object");
+
+						notification.respond({
+							x : 1,
+							y : 2
+						});
+					});
+
+					testInstance.publish("respond-test", {}, function (obj) {
+						expect(obj).to.deep.equal({
+							x : 1,
+							y : 2
+						});
+
+						done();
+					});
+				});
+			});
+
+			describe("n.dispatcher", function () {
+				it("should report the current target", function () {
+					testInstance.subscribe("dispatcher-test", function (notification) {
+						expect(notification.dispatcher).to.be.an("object");
+						expect(notification.dispatcher).to.deep.equal(testInstance);
+					});
+
+					testInstance.publish("dispatcher-test");
+				});
 			});
 
 		});
