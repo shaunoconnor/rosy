660a6a7f1a49e3bb592ccef6c40f813a8ff178b1
Richard Herrera <rich@doctyper.com>
2012-08-23 15:45:42 -0700

    Adding deep copy tests. One currently failing.

--- test/oxblood/core/Inheritance.js
+++ test/oxblood/core/Inheritance.js
@@ -45,7 +45,8 @@ define([
 					expect(foo.vars.x).to.equal(1);
 				});
 
-				it("should deep copy objects", function () {
+				describe("Deep Copying", function () {
+
 					var Foo = Page.extend({
 						vars : {
 							y : {
@@ -69,18 +70,37 @@ define([
 					var foo = new Foo();
 					var bar = new Bar();
 
-					expect(foo.vars.y.a).to.equal(1);
-					expect(foo.vars.y.b).to.equal(2);
-					expect(foo.vars.y.c[1]).to.equal(1);
+					it("should deep copy objects", function () {
+						expect(foo.vars.y.a).to.equal(1);
+						expect(foo.vars.y.b).to.equal(2);
+						expect(foo.vars.y.c[1]).to.equal(1);
 
-					expect(bar.vars.y.a).to.equal(3);
-					expect(bar.vars.y.b).to.equal(4);
-					expect(bar.vars.y.c[1]).to.equal(6);
+						expect(bar.vars.y.a).to.equal(3);
+						expect(bar.vars.y.b).to.equal(4);
+						expect(bar.vars.y.c[1]).to.equal(6);
+					});
 
-					bar.vars.y.b = 9;
+					it("should inherit parent values", function () {
+						var Baz = Bar.extend({
+							vars : {
+								x : true
+							}
+						});
 
-					expect(foo.vars.y.b).to.equal(2);
-					expect(bar.vars.y.b).to.equal(9);
+						var baz = new Baz();
+
+						expect(baz.vars.y.a).to.equal(3);
+						expect(baz.vars.y.b).to.equal(4);
+						expect(baz.vars.y.c[1]).to.equal(6);
+					});
+
+					it("should not manipulate parent values", function () {
+						bar.vars.y.b = 9;
+
+						expect(foo.vars.y.b).to.equal(2);
+						expect(bar.vars.y.b).to.equal(9);
+					});
+
 				});
 
 			});
