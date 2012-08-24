d8b6f7a2a2bfa55a7248bfeb1e1f7dcf81662e4e
Richard Herrera <rich@doctyper.com>
2012-08-23 17:36:57 -0700

    Revert "Updating libraries and config.js"
    This reverts commit 0681dc65bf073bd306e20bf529b8f67bd0554c16.
    
    Conflicts:
    	config.js
    	libs/plugins/amd/jquery-plugin.js

--- config.js
+++ config.js
@@ -2,16 +2,7 @@ require.config({
 
 	paths: {
 		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
-		"$plugin" : "libs/plugins/amd/jquery-plugin",
-		"History" : "libs/history.min",
-		"html5shiv" : "libs/html5shiv.min",
-		"Modernizr" : "libs/modernizr",
-		"swfobject" : "libs/swfobject",
-		"text" : "libs/plugins/amd/text",
-		"json" : "libs/plugins/amd/json",
-		"templates" : "../../templates",
-		"underscore": "libs/lodash",
-		"swig": "libs/swig.min"
+		"templates" : "../../templates"
 	},
 
 	waitSeconds: 15,
@@ -19,19 +10,6 @@ require.config({
 	shim : {
 		"$" : {
 			exports : "jQuery"
-		},
-		"swig": {
-			deps: ["underscore"],
-			exports : "swig"
-		},
-		"History": {
-			exports : "History"
-		},
-		"Modernizr": {
-			exports : "Modernizr"
-		},
-		"swfobject" : {
-			exports : "swfobject"
 		}
 	}
 });
