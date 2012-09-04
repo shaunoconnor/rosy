48d5049cc495e0d176500ad09502a472126f0427
Richard Herrera <rich@doctyper.com>
2012-09-04 10:29:57 -0700

    DOMClass now extends Class to use $.proxy over Function.prototype.bind when jQuery is present.

--- red/dom/DOMClass.js
+++ red/dom/DOMClass.js
@@ -2,14 +2,22 @@ define(
 
 	[
 		"../base/Class",
-		"./DOMManager"
+		"./DOMManager",
+		"$"
 	],
 
-	function (Class, DOMManager) {
+	function (Class, DOMManager, $) {
 
 		"use strict";
 
 		return Class.extend({
+
+			/**
+			* Shorthand for $.proxy(func, this)
+			*/
+			proxy : function (fn) {
+				return $.proxy(fn, this);
+			},
 
 			destroy : function () {
 				if (DOMManager) {
