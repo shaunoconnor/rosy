8064873ab0364471a51d32f076e106748199c89e
Richard Herrera <rich@doctyper.com>
2012-09-04 10:27:10 -0700

    Moving libs/plugins/amd/$.js -> libs/plugins/amd/jquery-plugin.js

--- libs/plugins/amd/jquery-plugin.js
+++ libs/plugins/amd/jquery-plugin.js
@@ -1,26 +1,22 @@
-(function(){
+(function () {
 
 	var prefix = "libs/plugins/jquery/jquery.";
 
 	define({
-
-	    load: function (name, req, load, config) {
-
-	    	req(['jquery'], function ($) {
-
-	    		if (!config.isBuild) {
+		load : function (name, req, load, config) {
+			req(["$"], function ($) {
+				if (!config.isBuild) {
 					req([prefix + name], function (val) {
 						load(val);
 					});
-				}
-				else {
+				} else {
 					load("");
 				}
 			});
 		},
 
 		loadFromFileSystem : function (plugin, name) {
-			var fs = nodeRequire('fs');
+			var fs = nodeRequire("fs");
 			var file = require.toUrl(prefix + name) + ".js";
 			var text = fs.readFileSync(file).toString();
 
@@ -30,7 +26,7 @@
 			return text;
 		},
 
-		write: function (pluginName, moduleName, write, config) {
+		write : function (pluginName, moduleName, write, config) {
 			write(this.loadFromFileSystem(pluginName, moduleName));
 		}
 	});
