8d69123d8bb12cf22243d13b1924ccf0e4285a05
Richard Herrera <rich@doctyper.com>
2012-09-04 10:28:21 -0700

    Adding config option for $plugin

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ require.config({
 
 	paths: {
 		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+		"$plugin": "libs/plugins/amd/jquery-plugin",
 		"templates" : "../../templates"
 	},
 
