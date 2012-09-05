c9cde4c5a39bba2a21707a0944bf8ecb3703513e
Richard Herrera <rich@doctyper.com>
2012-09-05 16:53:31 -0700

    Updating path to jQuery 1.8.

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require.config({
 
 	paths: {
-		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+		"$": "//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min",
+		// "$": "libs/jquery.js", // Replace above line to use local file
 		"$plugin": "libs/plugins/amd/jquery-plugin",
 		"templates" : "../../templates"
 	},
