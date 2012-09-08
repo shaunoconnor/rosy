44fa6dac0a0c531062714385d3cf9e98868ae137
Taka Kojima <taka.kojima@ff0000.com>
2012-09-07 17:20:08 -0700

    Fixing prefix in routes.js

--- __PROJECT_NAME__/config/routes.js
+++ __PROJECT_NAME__/config/routes.js
@@ -9,7 +9,7 @@ define(
 		"use strict";
 
 		var prefix = module.id.split("/");
-		prefix.splice(prefix.length - 3, 2, "");
+		prefix.splice(prefix.length - 2, 2, "");
 		prefix = prefix.join("/");
 
 		return {
